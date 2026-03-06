f7a997e6fa2e1163661cf7726f93c25f0183d3e5
Amjad Mowlana <amjadmoulana111@gmail.com>
2026-03-06 15:08:33 +0530

    limo booking changes

--- app/booking-limo-royale/Services.jsx
+++ app/booking-limo-royale/Services.jsx
@@ -143,7 +143,7 @@ export default function Services() {
                 </p>
 
                 <Link
-                  href="/online-reservations"
+                  href="/booking-limo-royale#quote"
                   className="inline-block text-sm uppercase tracking-wide text-[#bb9a4b] transition"
                 >
                   Book Now
