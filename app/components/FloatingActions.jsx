ae27a857263e213c638d4caaee314ee87848a31a
Ashutosh <ashutosh2801@gmail.com>
2026-04-17 18:15:45 +0530

    whatsapp no. changes

--- app/components/FloatingActions.jsx
+++ app/components/FloatingActions.jsx
@@ -6,14 +6,14 @@ const FloatingActions = () => {
     <div className="fixed bottom-25 md:bottom-15 right-5 flex-col items-end space-y-3 z-50 flex">
       
       {/* WhatsApp */}
-      <a href="https://wa.me/4167255466" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-2 text-white">
+      <a href="https://wa.me/+14167255466" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-2 text-white">
         <span className="bg-green-500 rounded-full shadow-lg hover:bg-green-600 transition p-3">
           <FaWhatsapp className="h-5 w-5" />
         </span>
       </a>
 
       {/* Phone */}
-      <a href="tel:4167255466" className="flex items-center space-x-2 text-white">
+      <a href="tel:+14167255466" className="flex items-center space-x-2 text-white">
         <span className="webBG rounded-full shadow-lg hover:bg-yellow-800 transition p-3">
           <PhoneIcon className="h-5 w-5" />
         </span>
